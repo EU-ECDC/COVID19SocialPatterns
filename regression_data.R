040a6c46a70460cf1df80c98f5820c0b845ef3c9
Anastasia Chatzilena <zd22230@bristol.ac.uk>
2024-04-26 02:18:56 +0100

    Add percentile input

--- regression_data.R
+++ regression_data.R
@@ -1,48 +1,106 @@
 #Dependent
 
-fitted_beta_coefficients <- function(country){
+fitted_beta_coefficients <- function(country,percentile){
 
 loaded_fit <- load_fit(country)
 posts <-  rstan::extract(loaded_fit)
-median_beta1 = apply(posts$beta1, 2, median)
-median_beta2 = apply(posts$beta2, 2, median)
-median_beta3 = apply(posts$beta3, 2, median)
-median_beta4 = apply(posts$beta4, 2, median)
-dependent <- cbind(median_beta1,median_beta2,median_beta3,median_beta4)
+median_beta1 = c(apply(posts$beta1, 2, median),NA)
+median_beta2 = c(apply(posts$beta2, 2, median),NA)
+median_beta3 = c(apply(posts$beta3, 2, median),NA)
+median_beta4 = c(apply(posts$beta4, 2, median),NA)
 
-comix_date_ranges <- comix_dates(country)
-start_date <- min(comix_date_ranges[,1])
-end_date <- max(comix_date_ranges[,2])
-dates <- seq(start_date, end_date, by = "days")
+low_beta1 = c(apply(posts$beta1, 2, quantile, probs = c(0.025)),NA)
+low_beta2 = c(apply(posts$beta2, 2, quantile, probs = c(0.025)),NA)
+low_beta3 = c(apply(posts$beta3, 2, quantile, probs = c(0.025)),NA)
+low_beta4 = c(apply(posts$beta4, 2, quantile, probs = c(0.025)),NA)
 
-# Create a data frame with dates
-dependent_ext <- data.frame(Date = dates)
-dependent_ext$beta1 <- NA
-dependent_ext$beta2 <- NA
-dependent_ext$beta3 <- NA
-dependent_ext$beta4 <- NA
+high_beta1 = c(apply(posts$beta1, 2, quantile, probs = c(0.975)),NA)
+high_beta2 = c(apply(posts$beta2, 2, quantile, probs = c(0.975)),NA)
+high_beta3 = c(apply(posts$beta3, 2, quantile, probs = c(0.975)),NA)
+high_beta4 = c(apply(posts$beta4, 2, quantile, probs = c(0.975)),NA)
 
-# Linearly interpolate betas based on comix date ranges
-for (i in 1:(nrow(comix_date_ranges) - 1)) {
-  date_range_start <- comix_date_ranges[i, ]
-  date_range_end <- comix_date_ranges[i + 1, ]
-  
-  indices <- dependent_ext$Date >= date_range_start$start_date & dependent_ext$Date <= date_range_end$end_date
-  
-  # Linear interpolation for each beta
-  alpha <- as.numeric(dependent_ext$Date[indices] - date_range_start$start_date) / as.numeric(date_range_end$end_date - date_range_start$start_date)
-  dependent_ext$beta1[indices] <- (1 - alpha) * median_beta1[i] + alpha * median_beta1[i + 1]
-  dependent_ext$beta2[indices] <- (1 - alpha) * median_beta2[i] + alpha * median_beta2[i + 1]
-  dependent_ext$beta3[indices] <- (1 - alpha) * median_beta3[i] + alpha * median_beta3[i + 1]
-  dependent_ext$beta4[indices] <- (1 - alpha) * median_beta4[i] + alpha * median_beta4[i + 1]
-  
-  # # Assign constant beta values for each date range
-  # dependent_ext$beta1[indices] <- median_beta1[i]
-  # dependent_ext$beta2[indices] <- median_beta2[i]
-  # dependent_ext$beta3[indices] <- median_beta3[i]
-  # dependent_ext$beta4[indices] <- median_beta4[i]
+if (percentile=="50"){
+  dependent <- cbind(median_beta1,median_beta2,median_beta3,median_beta4)
+}else if (percentile=="2.5"){
+  dependent <- cbind(low_beta1,low_beta2,low_beta3,low_beta4)
+}else if (percentile=="97.5"){
+dependent <- cbind(high_beta1,high_beta2,high_beta3,high_beta4)
 }
 
+times <- c(comix_dates(country)[,1],max(comix_dates(country)[,2]))
+ext_dates <- seq(min(comix_dates(country)[,1]), max(comix_dates(country)[,2]), by = "days")
+numeric_dates <- as.numeric(times - min(times)) + 1
+wave <- c(1:length(times))
+
+df1 <- tibble(x=numeric_dates,y=dependent[,1],wave)
+df2 <- tibble(x=numeric_dates,y=dependent[,2],wave)
+df3 <- tibble(x=numeric_dates,y=dependent[,3],wave)
+df4 <- tibble(x=numeric_dates,y=dependent[,4],wave)
+
+df1 %>% 
+  mutate(gradient = (lead(y)-y)/(lead(x)-x)) %>% 
+  complete(x = x:max(x)) %>% 
+  fill(wave) %>% 
+  group_by(wave) %>% 
+  fill(gradient)%>% 
+  mutate(y2=first(y) + (x-first(x)) *gradient)%>%
+  mutate(y2=ifelse(!is.na(y2),y2,y))->df1_ext
+
+df2 %>% 
+  mutate(gradient = (lead(y)-y)/(lead(x)-x)) %>% 
+  complete(x = x:max(x)) %>% 
+  fill(wave) %>% 
+  group_by(wave) %>% 
+  fill(gradient)%>% 
+  mutate(y2=first(y) + (x-first(x)) *gradient)%>%
+  mutate(y2=ifelse(!is.na(y2),y2,y))->df2_ext
+
+df3 %>% 
+  mutate(gradient = (lead(y)-y)/(lead(x)-x)) %>% 
+  complete(x = x:max(x)) %>% 
+  fill(wave) %>% 
+  group_by(wave) %>% 
+  fill(gradient)%>% 
+  mutate(y2=first(y) + (x-first(x)) *gradient)%>%
+  mutate(y2=ifelse(!is.na(y2),y2,y))->df3_ext
+
+df4 %>% 
+  mutate(gradient = (lead(y)-y)/(lead(x)-x)) %>% 
+  complete(x = x:max(x)) %>% 
+  fill(wave) %>% 
+  group_by(wave) %>% 
+  fill(gradient)%>% 
+  mutate(y2=first(y) + (x-first(x)) *gradient)%>%
+  mutate(y2=ifelse(!is.na(y2),y2,y))->df4_ext
+
+dependent_ext <- tibble(ext_dates,
+                        df1_ext$y, df1_ext$y2,
+                        df2_ext$y,df2_ext$y2,
+                        df3_ext$y,df3_ext$y2,
+                        df4_ext$y,df4_ext$y2)%>% 
+  filter(!is.na(df1_ext$y2))%>%
+  rename(Date = ext_dates,
+         beta1_w = `df1_ext$y`,
+         beta1 = `df1_ext$y2`,
+         beta2_w = `df2_ext$y`,
+         beta2 = `df2_ext$y2`,
+         beta3_w = `df3_ext$y`,
+         beta3 = `df3_ext$y2`,
+         beta4_w = `df4_ext$y`,
+         beta4 = `df4_ext$y2`,)
+
+# dependent_ext %>% ggplot()+
+#   geom_point(aes(x=ext_dates,y=`df1_ext$y`),colour='red',size=4)+
+#   geom_point(aes(x=ext_dates,y=`df1_ext$y2`))
+# dependent_ext %>% ggplot()+
+#   geom_point(aes(x=ext_dates,y=`df2_ext$y`),colour='red',size=4)+
+#   geom_point(aes(x=ext_dates,y=`df2_ext$y2`))
+# dependent_ext %>% ggplot()+
+#   geom_point(aes(x=ext_dates,y=`df3_ext$y`),colour='red',size=4)+
+#   geom_point(aes(x=ext_dates,y=`df3_ext$y2`))
+# dependent_ext %>% ggplot()+
+#   geom_point(aes(x=ext_dates,y=`df4_ext$y`),colour='red',size=4)+
+#   geom_point(aes(x=ext_dates,y=`df4_ext$y2`))
 
 return(dependent_ext)
 }
@@ -89,14 +147,17 @@ NPI_PD <- NPI_PD %>%
                             TRUE ~ .))) %>%
   mutate(across(where(is.numeric), as.character))
 
-comix_date_ranges <- comix_dates(country)
-NPI_PD_comix <- NPI_PD %>%
-  filter(date>=min(comix_date_ranges[,1]))%>% 
-  filter(date<=max(comix_date_ranges[,2]))
+# NPI_PD_comix <- NPI_PD %>%
+#   filter(date>=min(comix_dates(country)[,1]))%>% 
+#   filter(date<=max(comix_dates(country)[,1]))
 
-NPI_PD_comix <- NPI_PD_comix[, -which(names(NPI_PD_comix) %in% c("date"))]
-NPI_PD_var <- NPI_PD_comix%>%
-  select(where(~ length(unique(.)) > 1))
+# NPI_PD_comix <- NPI_PD_comix[, -which(names(NPI_PD_comix) %in% c("date"))]
+# NPI_PD_var <- NPI_PD_comix%>%
+#   select(where(~ length(unique(.)) > 1))
+
+NPI_PD <- NPI_PD[, -which(names(NPI_PD) %in% c("date"))]
+NPI_PD_var <- NPI_PD%>%
+  dplyr::select(where(~ length(unique(.)) > 1))
 
 # apply MCA
 mca1 = MCA(NPI_PD_var, graph = FALSE)
@@ -107,17 +168,56 @@ if (ncol(NPI_PD_var)>=5){
   independent <- as.data.frame(mca1$ind$coord[, 1:4])
 }
 
+independent <- cbind(NPI$date,independent)
+
 return(independent)
 }
 
 
-reg_data_to_stan_list <- function(country){
-
-  reg_data = list(n_obs = nrow(fitted_beta_coefficients(country)),
-                  K = ncol(fitted_beta_coefficients(country))-1,  #number of regressions
-                  J = ncol(mca_factors(country)), #number of covariates
-                  x = mca_factors(country),
-                  y = fitted_beta_coefficients(country)[,-1])
+fitted_interc <- function(country,percentile){
   
+  loaded_fit <- load_fit(country)
+  posts <-  rstan::extract(loaded_fit)
+
+  if (percentile=="50"){
+    fit_interc <- apply(posts$beta0, 2, median)
+  }else if (percentile=="2.5"){
+    fit_interc <- apply(posts$beta0, 2, quantile, probs = c(0.025))
+  }else if (percentile=="97.5"){
+    fit_interc <- apply(posts$beta0, 2, quantile, probs = c(0.975))
+  }
+  return(fit_interc)
+}
+
+
+# reg_data_to_stan_list <- function(country){
+# 
+#   reg_data = list(n_obs = nrow(fitted_beta_coefficients(country)),
+#                   K = ncol(fitted_beta_coefficients(country)[,-c(1,2,4,6,8)]),  #number of regressions
+#                   J = ncol(mca_factors(country)), #number of covariates
+#                   x = mca_factors(country),
+#                   y = fitted_beta_coefficients(country)[,-c(1,2,4,6,8)]
+#                   )
+#   
+#   return(reg_data)
+# }
+
+reg_data_to_stan_list <- function(country, percentile){
+
+  reg_data = list(n_train = nrow((mca_factors(country) %>%
+                                    filter(`NPI$date`>=min(comix_dates(country)[,1]))%>%
+                                    filter(`NPI$date`<=max(comix_dates(country)[,1])))[,-1]),
+                  n_pred = nrow((mca_factors(country))[,-1]),
+                  K = ncol(fitted_beta_coefficients(country,percentile)[,-c(1,2,4,6,8)]),  #number of regressions
+                  J = ncol(mca_factors(country))-1,  #number of factor dimensions
+                  I = ncol(contact_data(country)[[2]][,-1]),
+                  x_train= (mca_factors(country) %>%
+                            filter(`NPI$date`>=min(comix_dates(country)[,1]))%>%
+                            filter(`NPI$date`<=max(comix_dates(country)[,1])))[,-1],
+                  x_pred = (mca_factors(country))[,-1],
+                  beta_train = fitted_beta_coefficients(country,percentile)[,-c(1,2,4,6,8)],
+                  beta0_train = fitted_interc(country,percentile),
+                  X = t(as.matrix(contact_data(country)[[1]][,])))
+
   return(reg_data)
 }
