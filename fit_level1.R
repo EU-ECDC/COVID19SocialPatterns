3dd958fb56c172b5c817d6e2d9b27362b9cce4b4
Anastasia Chatzilena <zd22230@bristol.ac.uk>
2024-04-26 02:03:45 +0100

    Add model argument; Remove hyperparameters

--- fit_level1.R
+++ fit_level1.R
@@ -1,8 +1,7 @@
-nuts_fit <- function(country, n_chains, n_warmups, n_iter, n_thin, 
-                     n_adapt_delta,n_max_treedepth,aa,tt,ss){
-  fit_by_country <-  stan("m1_penalised_test.stan",
-                          #"m1_penalised.stan", 
-                          data = data_to_stan_list(country,aa,tt,ss), 
+nuts_fit <- function(model, country, n_chains, n_warmups, n_iter, n_thin, 
+                     n_adapt_delta,n_max_treedepth){
+  fit_by_country <-  stan(model,
+                          data = data_to_stan_list(country), 
                           pars = parameters , 
                           #init = inits(country), 
                           chains = n_chains, 
@@ -15,15 +14,13 @@ nuts_fit <- function(country, n_chains, n_warmups, n_iter, n_thin,
 }
 
 load_fit <- function(country) {
-  valid_countries <- c("AT", "BE","BE_ext", "HR", "DK", "EE", "FI", "FR", "GR", "HU", 
+  valid_countries <- c("AT", "BE", "HR", "DK", "EE", "FI", "FR", "GR", "HU",
                        "IT", "LT", "NL", "PL", "PT", "SK", "SI", "ES", "CH")
   
   if (!country %in% valid_countries) {
     stop("Invalid country. Please choose from: ", paste(valid_countries, collapse = ", "))
   }
-  
-  #data_file <- paste0('C:/Users/zd22230/OneDrive - University of Bristol/ContactPatternsAC/Rfiles/TVEM_results/knots10/',country, '_TVEM.RData')
-  data_file <- paste0('C:/Users/zd22230/OneDrive - University of Bristol/ContactPatternsAC/Rfiles/TVEM_results/knots50/',country, '_TVEM.RData')
+  data_file <- paste0('C:/Users/zd22230/OneDrive - University of Bristol/ContactPatternsAC/Rfiles/TVEM_results/knots10_interc/',country, '_TVEM.RData')
   
   if (file.exists(data_file)) {
     loaded_env <- new.env()
